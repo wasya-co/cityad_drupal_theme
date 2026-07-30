4b5c2c8e06ecfc143e5ddf015b1dedfbcf9cde15
mac_a2141 <victor@piousbox.com>
2026-07-30 17:01:08 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+
+
+   LC_ALL=C find . -type f -exec sed -i '' 's/bootstrap_barrio_subtheme/cityad_drupal_theme/g' {} +
---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.cityad_drupal_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
